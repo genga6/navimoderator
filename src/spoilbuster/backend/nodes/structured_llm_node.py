09eb145d877e63fee2c937d3e68ae5bd5b3bbc7b
genga6 <gengaru617science@gmail.com>
2025-01-01 14:13:23 +0000

    Update: to implement youtube_listener_node.py

---
+++ src/spoilbuster/backend/nodes/structured_llm_node.py
@@ -0,0 +1,62 @@
+import ast
+from spoilbuster.backend.core.node import Node
+from litellm import completion
+from jinja2 import Environment
+from pydantic import BaseModel, create_model
+
+
+class DynamicModel(BaseModel):
+    pass
+
+
+class StructuredLLMNode(Node):
+    def __init__(
+        self,
+        input_key: list[str],
+        output_key: list[str],
+        llm_name: str,
+        prompt_template: str,
+    ):
+        super().__init__(input_key, output_key)
+        self.llm_name = llm_name
+        self.prompt_template = prompt_template
+        self.dynamicmodel = self._create_dynamic_model(DynamicModel)
+
+    def _create_dynamic_model(self, base_model: BaseModel):
+        default_type = str
+        default_required = ...
+        fields = {field: (default_type, default_required) for field in self.output_key}
+        return create_model(
+            base_model.__name__,
+            **fields,
+            __base__=base_model,
+        )
+
+    def litellm_output(self, llm_name: str, prompt: str, response_field) -> dict:
+        response = completion(
+            model=llm_name,
+            messages=[
+                {"role": "user", "content": prompt},
+            ],
+            response_format=response_field,
+        )
+        output = response.choices[0].message.content
+        output_dict = ast.literal_eval(output)
+        return output_dict
+
+    def execute(self, state) -> dict:
+        data = {key: state[key] for key in self.input_key}
+
+        env = Environment()
+        template = env.from_string(self.prompt_template)
+        prompt = template.render(data)
+
+        result_dict = {}
+        if self.llm_name == "gpt-4o":    #TODO: Expand the model
+            result_dict = self.litellm_output(self.llm_name, prompt, self.dynamicmodel)
+        elif self.llm_name == "gpt-3.5-turbo":
+            result_dict = self.litellm_output(self.llm_name, prompt, self.dynamicmodel)
+        else:
+            raise ValueError("LLM not supported")
+            
+        return {**result_dict}
---
+++ tests/test_nodes/test_youtube_listener_node.py
@@ -0,0 +1,45 @@
+import pytest
+from spoilbuster.backend.nodes.youtube_listener_node import YoutubeListenerNode
+from langgraph.graph import StateGraph
+from typing import TypedDict
+from unittest.mock import patch
+
+class State(TypedDict):
+    video_id: str
+    messages: list[str]
+    
+
+@pytest.fixture
+def mock_youtube_listener():
+    with patch("spoilbuster.backend.nodes.youtube_listener_node.YoutubeListenerNode._fetch_live_chat_messages") as mock_fetch:
+        mock_fetch.return_value = [
+            "This is a test comment.",
+            "Another test comment."
+        ]
+        yield mock_fetch
+
+def test_youtube_listener_node(mock_youtube_listener):
+    input_key = ["video_id"]
+    output_key = ["messages"]
+
+    graph_builder = StateGraph(State)
+    graph_builder.add_node(
+        "youtube_listener_node",
+        YoutubeListenerNode(
+            input_key=input_key,
+            output_key=output_key,
+        ),
+    )
+    graph_builder.set_entry_point("youtube_listener_node")
+    graph_builder.set_finish_point("youtube_listener_node")
+    graph = graph_builder.compile()
+
+    state = {
+        "video_id": "test_video_id",
+    }
+    result_state = graph.invoke(state, debug=True)
+
+    assert "messages" in result_state, "Messages key should exist in the result state."
+    assert len(result_state["messages"]) == 2, "There should be 2 messages in the result."
+    assert result_state["messages"][0] == "This is a test comment."
+    assert result_state["messages"][1] == "Another test comment."
